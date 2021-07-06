d0a2431bf71af7e18e907b2bf179ad16fc157970
AIA <AIA@AIA>
2021-07-06 14:14:13 +0900

    User02 : TestProductApp.java 생성함.
    ~~~~~

---
+++ z.testPJT/src/test/java/product/TestProductApp.java
@@ -0,0 +1,10 @@
+package product;
+
+public class TestProductApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("User02 문가를 함.....");
+	}
+
+}
